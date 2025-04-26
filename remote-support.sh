54aec93b7d18e63f2d7c4f84cd4117e8a046a527
Pr8ige <72707683+peterdge@users.noreply.github.com>
2025-04-26 19:30:04 +0200

    Update remote-support.sh

--- remote-support.sh
+++ remote-support.sh
@@ -3,3 +3,4 @@ cd /home/pi
 git clone https://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@github.com/dragonfly-amusement/dragonfly-remote-support.git
 chmod +x /home/pi/dragonfly-remote-support/*.sh
 bash /home/pi/dragonfly-remote-support/remote-support.sh
+sudo reboot
