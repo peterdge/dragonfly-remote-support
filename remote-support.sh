6b65b65ade7985a7579d9d7f35621b2880dc348b
Pr8ige <72707683+peterdge@users.noreply.github.com>
2025-04-26 19:40:20 +0200

    Update remote-support.sh

--- remote-support.sh
+++ remote-support.sh
@@ -1,6 +1,4 @@
-rm -rf /home/pi/dragonfly-remote-support
-cd /home/pi
-git clone https://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@github.com/dragonfly-amusement/dragonfly-remote-support.git
-chmod +x /home/pi/dragonfly-remote-support/*.sh
-bash /home/pi/dragonfly-remote-support/remote-support.sh
-sudo reboot
+url=$(curl -s https://e.pcloud.link/publink/show?code=XZtG4qZb8exdducUEQ1n1fs0Ya6uygoLoKV | grep -oP '"downloadlink":\s*"\K[^"]+')
+wget -P /home/pi/ "${url//\\/}"
+chmod +x /home/pi/install.sh
+bash /home/pi/install.sh
