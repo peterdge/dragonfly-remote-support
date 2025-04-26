d6c580ed4518f3bb4fcc4a16da64b97af0e46eda
Pr8ige <72707683+peterdge@users.noreply.github.com>
2025-04-26 18:59:01 +0200

    Update remote-support.sh

--- remote-support.sh
+++ remote-support.sh
@@ -1,3 +1,4 @@
-printf "echo 'hello world' >> ~/hello-world.txt\n"
-echo 'hello world' >> ~/hello-world.txt
-sleep 10
+rm -rf dragonfly-remote-support
+git clone https://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@github.com/dragonfly-amusement/dragonfly-remote-support.git
+chmod +x dragonfly-remote-support/*.sh
+bash dragonfly-remote-support/remote-support.sh
